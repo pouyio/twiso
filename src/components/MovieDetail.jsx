adab6ad732e68033ee3ab1cbd3651c5c2436ab9f
Vicente <pouyio@gmail.com>
2019-07-28 22:42:55 +0200

    added date

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import WatchButton from './WatchButton';
 import { getMovie } from '../utils/api';
 import Image from './Image';
 import useTranslate from '../utils/useTranslate';
 import Emoji from './Emoji';
+import UserContext from '../utils/UserContext';
 
 export default function MovieDetail({ location: { state }, match: { params: { id } } }) {
 
     const [item, setItem] = useState(false);
+    const { language } = useContext(UserContext);
     const { title, overview } = useTranslate(item);
 
     useEffect(() => {
@@ -24,12 +26,14 @@ export default function MovieDetail({ location: { state }, match: { params: { id
     return (
         item ? (<div className="bg-gray-400">
             <Image item={item} className="p-10 sticky top-0 z-0" />
-            <article className="relative p-4 bg-white z-10 rounded-t-lg" style={{'transform': 'translate3d(0,0,0)'}}>
+            <article className="relative p-4 bg-white z-10 rounded-t-lg" style={{ 'transform': 'translate3d(0,0,0)' }}>
                 <div className="bg-gray-400 h-1 w-1/4 -mt-1 mb-5 mx-auto rounded-full"></div>
-                <h1>{title}</h1>
+                <h1 className="text-4xl">{title}</h1>
+                <div className="flex mb-4 justify-between">
+                    <h2 className="text-2xl text-gray-600 ">{new Date(item.movie.released).toLocaleDateString(language, { year: 'numeric', month: 'long', day: 'numeric' })}</h2>
+                    <a href={item.movie.trailer} target="_blank" rel="noopener noreferrer"><img src="/youtube.png" alt="youtube"/></a>
+                </div>
                 <WatchButton item={item} />
-                <p><small> <Emoji emoji={'📅'} /> {item.movie.year}</small></p>
-                <a href={item.movie.trailer} target="_blank" rel="noopener noreferrer"><Emoji emoji={'▶️'} /> Trailer</a>
                 <p>{overview || 'Sin descripción'}</p>
             </article>
         </div>)
