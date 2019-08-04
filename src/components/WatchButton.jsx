cfcc3d684fd415cce5c5a0e72b12fa4a4c6d649b
Vicente <pouyio@gmail.com>
2019-08-04 13:01:17 +0200

    added more button styles

--- src/components/WatchButton.jsx
+++ src/components/WatchButton.jsx
@@ -27,14 +27,14 @@ export default function WatchButton({ item }) {
     return (
         <div className="flex justify-around my-8">
             {isWatched() ?
-                <button className="bg-green-400 py-3 px-12 rounded-full text-white" onClick={() => removeMovieWatched(item)}>
+                <button className="bg-green-400 py-3 px-12 rounded-full text-white font-bold" onClick={() => removeMovieWatched(item)}>
                     ✓ Vista</button>
-                : <button className="bg-gray-200 py-3 px-12 rounded-full text-gray-700" onClick={() => addMovieWatched(item)}>
+                : <button className="bg-gray-200 py-3 px-12 rounded-full text-gray-700 font-thin" onClick={() => addMovieWatched(item)}>
                     Vista</button>}
             {isWatchlist() ?
-                <button className="bg-blue-400 py-3 px-12 rounded-full text-white" onClick={() => removeMovieWatchlist(item)}>
+                <button className="bg-blue-400 py-3 px-12 rounded-full text-white font-bold" onClick={() => removeMovieWatchlist(item)}>
                     Pendiente</button>
-                : <button className="bg-gray-200 py-3 px-12 rounded-full text-gray-700" onClick={() => addMovieWatchlist(item)}>
+                : <button className="bg-gray-200 py-3 px-12 rounded-full text-gray-700 font-thin" onClick={() => addMovieWatchlist(item)}>
                     Pendiente</button>}
         </div>
     )
