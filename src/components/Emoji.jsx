afa64ac1969ede00b4f4260b3c1612f2dc61f15b
Vicente <pouyio@gmail.com>
2019-08-04 19:38:45 +0200

    added rating

--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Emoji({ emoji, rotating, className , ...props}) {
+export default function Emoji({ emoji, rotating, className = '' , ...props}) {
 
     return <span {...props} className={className + ' ' + (rotating ? 'spin' : '')} role="img" aria-label="emoji">{emoji}</span>;
 
--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -43,13 +43,18 @@ export default function MovieDetail({ location: { state }, match: { params: { id
 
     return (
         item ? (<div className={getBgClassName()}>
-            <Image item={item} className="p-10 sticky top-0 z-0" style={{ minHeight: '15em' }} />
+            <div className="p-10 sticky top-0 z-0" style={{ minHeight: '15em' }}>
+                <Image item={item} />
+                <a className="absolute" style={{right: '4em', bottom: '4em'}} href={item.movie.trailer} target="_blank" rel="noopener noreferrer">
+                    <Emoji emoji="▶️" className="text-4xl"/>
+                </a>
+            </div>
             <article className="relative p-4 bg-white z-10 rounded-t-lg" style={{ 'transform': 'translate3d(0,0,0)' }}>
                 <div className="bg-gray-400 h-1 w-1/4 -mt-1 mb-5 mx-auto rounded-full"></div>
-                <h1 className="text-4xl leading-none">{title}</h1>
-                <div className="flex mb-4 justify-between">
-                    <h2 className="text-2xl text-gray-600 ">{new Date(item.movie.released).toLocaleDateString(language, { year: 'numeric', month: 'long', day: 'numeric' })}</h2>
-                    <a href={item.movie.trailer} target="_blank" rel="noopener noreferrer"><img src="/youtube.png" alt="youtube" /></a>
+                <h1 className="text-4xl leading-none text-center mb-2">{title}</h1>
+                <div className="flex mb-4 justify-between items-center text-gray-600">
+                    <h2 className="text-2xl ">{new Date(item.movie.released).toLocaleDateString(language, { year: 'numeric', month: 'long', day: 'numeric' })}</h2>
+                    <p className="font-thin"><Emoji emoji="❤️" />{Math.round(item.movie.rating * 10)}%</p>
                 </div>
                 <WatchButton item={item} />
 
