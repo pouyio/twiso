ef4391c4fa97280a56a95697727c9c676a245c86
Vicente <pouyio@gmail.com>
2019-08-09 16:36:02 +0200

    added counter

--- src/components/Watched.jsx
+++ src/components/Watched.jsx
@@ -20,7 +20,7 @@ export default function Watched() {
     return (
         <div>
             {globalError && <div><pre className="overflow-scroll text-xs text-red-700 whitespace-pre-wrap">{JSON.stringify(globalError)}</pre></div>}
-            <h1 className="text-3xl text-center text-gray-700 m-4"><Emoji emoji="📚" /> Vistas</h1>
+            <h1 className="text-3xl text-center text-gray-700 m-4"><Emoji emoji="📚" /> Vistas {movies.length}</h1>
             <PaginationContainer items={movies}>
                 <ul className="flex flex-wrap p-2 justify-center">
                     {getMoviesByPage(currentPage).map(m => <li key={m.movie.ids.trakt} style={{ flex: '1 0 45%', maxWidth: '15em' }}>
