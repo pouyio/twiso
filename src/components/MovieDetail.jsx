508ebc5ab342a32daab4aa1472ba9f8553b82a47
Vicente <pouyio@gmail.com>
2019-08-18 18:20:46 +0200

    show original title on click

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -11,6 +11,7 @@ import Genres from './Genres';
 export default function MovieDetail({ location: { state }, match: { params: { id } } }) {
 
     const [item, setItem] = useState(false);
+    const [showOriginalTitle, setShowOriginalTitle] = useState(false);
     const { language,
         isWatched,
         isWatchlist } = useContext(UserContext);
@@ -41,17 +42,23 @@ export default function MovieDetail({ location: { state }, match: { params: { id
         return 'bg-gray-300';
     }
 
+    const toggleShowOriginalTitle = () => {
+        setShowOriginalTitle(!showOriginalTitle);
+    }
+
     return (
         item ? (<div className={getBgClassName()}>
             <div className="p-10 sticky top-0 z-0" style={{ minHeight: '15em' }}>
-                <Image item={item} />
+                <Image item={item} type="movie"/>
                 {item.movie.trailer && <a className="absolute" style={{ right: '4em', bottom: '4em' }} href={item.movie.trailer} target="_blank" rel="noopener noreferrer">
                     <Emoji emoji="▶️" className="text-4xl" />
                 </a>}
             </div>
             <article className="relative p-4 bg-white z-10 rounded-t-lg" style={{ 'transform': 'translate3d(0,0,0)' }}>
                 <div className="bg-gray-400 h-1 w-1/4 -mt-1 mb-5 mx-auto rounded-full"></div>
-                <h1 className="text-4xl leading-none text-center mb-4">{title}</h1>
+                <h1 onClick={toggleShowOriginalTitle} className="text-4xl leading-none text-center mb-4">
+                    {showOriginalTitle ? item.movie.title : title}
+                </h1>
                 <div className="flex mb-4 justify-between items-center text-gray-600">
                     <h2>{new Date(item.movie.released).toLocaleDateString(language, { year: 'numeric', month: 'long', day: 'numeric' })}</h2>
                     <h2>{item.movie.runtime} mins</h2>
