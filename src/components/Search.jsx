067ba25860db4aae96c1dbae8d7b3202d76c6ca6
Vicente <pouyio@gmail.com>
2019-09-07 02:43:51 +0200

    fix overflow on safari

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -55,7 +55,7 @@ export default function Search() {
             {search || movieResults.length ?
                 <>
                     <h1 className="text-3xl mt-4 text-gray-700">Películas </h1>
-                    <ul className="-mx-2 -mt-2 flex overflow-x-auto">
+                    <ul className="-mx-2 -mt-2 flex overflow-x-auto" style={{ WebkitOverflowScrolling: 'touch' }}>
                         {movieResults.map(r => <li key={r.movie.ids.slug} className="p-2" style={{ flex: '1 0 50%', maxWidth: '15em' }}>
                             <ImageLink item={r} style={{ minHeight: '10em' }} type="movie" />
                         </li>)}
@@ -66,7 +66,7 @@ export default function Search() {
             {(search || showResults.length) ?
                 <>
                     <h1 className="text-3xl mt-4 text-gray-700">Series </h1>
-                    <ul className="-mx-2 -mt-2 flex overflow-x-auto">
+                    <ul className="-mx-2 -mt-2 flex overflow-x-auto" style={{ WebkitOverflowScrolling: 'touch' }}>
                         {showResults.map(r => <li key={r.show.ids.slug} className="p-2" style={{ flex: '1 0 50%', maxWidth: '15em' }}>
                             <ImageLink item={r} style={{ minHeight: '10em' }} type="show" />
                         </li>)}
@@ -77,7 +77,7 @@ export default function Search() {
             {(search || peopleResults.length) ?
                 <>
                     <h1 className="text-3xl mt-4 text-gray-700">Personas </h1>
-                    <ul className="-mx-2 -mt-2 flex overflow-x-auto">
+                    <ul className="-mx-2 -mt-2 flex overflow-x-auto" style={{ WebkitOverflowScrolling: 'touch' }}>
                         {peopleResults.map(r => <li key={r.person.ids.slug} className="p-2" style={{ flex: '1 0 50%', maxWidth: '15em' }}>
                             <ImageLink item={r} style={{ minHeight: '10em' }} type="person" />
                         </li>)}
