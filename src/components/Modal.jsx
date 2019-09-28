e58cfa7a0e3cd7ad559d3506736bc00dca78e6b6
Vicente <pouyio@gmail.com>
2019-09-28 13:05:24 +0200

    fix modal theme

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,23 @@
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import ModalContext from '../utils/ModalContext';
+import ThemeContext from '../utils/ThemeContext';
 
 const Modal = ({ modalRef }) => {
   const { isShowing, toggle, title, text } = useContext(ModalContext);
+  const { theme } = useContext(ThemeContext);
 
   return ReactDOM.createPortal(
     <div
       onClick={toggle}
-      className={`${
+      className={`${theme} ${
         isShowing ? '' : 'hidden'
         } fixed w-full h-full top-0 left-0 flex items-center justify-center z-10`}
     >
       <div className="absolute w-full h-full bg-gray-900 opacity-50"></div>
 
       <div
-        className="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto"
+        className="text-black bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto"
         style={{ maxHeight: '60vh', WebkitOverflowScrolling: 'touch' }}
       >
         <div className="p-4 leading-tight">
