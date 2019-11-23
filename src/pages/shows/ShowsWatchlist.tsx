2c7f073de9d44f3357fa8a16e20bdbe398686ca4
Vicente Ortiz <vicente@MacBook-Pro-de-Vicente.local>
2019-11-24 00:15:15 +0100

    improved sorting

--- src/pages/shows/ShowsWatchlist.tsx
+++ src/pages/shows/ShowsWatchlist.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ImageLink from '../../components/ImageLink';
 import PaginationContainer from '../../components/Pagination/PaginationContainer';
 import usePagination from '../../utils/usePagination';
 import { useGlobalState } from '../../state/store';
+import { ShowWatchlist } from 'models';
 
 const ShowsWatchlist: React.FC = () => {
+  const [orderedShows, setOrderedShows] = useState<ShowWatchlist[]>([]);
   const {
     state: {
       userInfo: {
@@ -14,9 +16,30 @@ const ShowsWatchlist: React.FC = () => {
   } = useGlobalState();
   const { getItemsByPage } = usePagination(watchlist);
 
+  useEffect(() => {
+    const nearFuture = new Date();
+    nearFuture.setDate(nearFuture.getDate() + 7);
+    const newItems = watchlist
+      .sort((a, b) =>
+        new Date(a.listed_at!) < new Date(b.listed_at!) ? -1 : 1,
+      )
+      .reduce((acc: ShowWatchlist[], m) => {
+        if (!m.show.first_aired) {
+          return [...acc, m];
+        }
+        const released = new Date(m.show.first_aired);
+        if (released < nearFuture) {
+          return [m, ...acc];
+        } else {
+          return [...acc, m];
+        }
+      }, []);
+    setOrderedShows(newItems);
+  }, [watchlist]);
+
   return (
     <div>
-      <PaginationContainer items={watchlist}>
+      <PaginationContainer items={orderedShows}>
         <ul className="flex flex-wrap p-2 items-stretch justify-center">
           {getItemsByPage().map(m => (
             <li
