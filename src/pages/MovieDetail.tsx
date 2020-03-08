44d5a98a6cd2d1b7ed86ec2cba3dbfe126e3a9c1
Vicente Ortiz <vicente@MacBook-Pro-de-Vicente.local>
2020-03-08 19:23:37 +0100

    removed useless open-graph meta tags

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -81,12 +81,6 @@ export default function MovieDetail() {
     <div className={getBgClassName()}>
       <Helmet>
         <title>{title}</title>
-        <meta property="og:title" content={title || item.title} />
-        <meta
-          property="og:description"
-          content={(overview || 'Sin descripción').slice(0, 140)}
-        />
-        <meta property="og:url" content={window.location.href} />
       </Helmet>
       <div className="lg:max-w-5xl lg:mx-auto">
         <div
