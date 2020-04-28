203d05fa52d3b1e8216730fdefb2e55522d51b80
Vicente <pouyio@gmail.com>
2020-04-28 21:57:21 +0200

    limit some requests

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// import rateLimit from 'axios-rate-limit';
+import rateLimit from 'axios-rate-limit';
 import { Session } from '../contexts/AuthContext';
 import {
   MovieWatchlist,
@@ -24,7 +24,7 @@ import {
   ShowWatched,
   MovieWatched,
   Ratings,
-  Profile
+  Profile,
 } from '../models';
 
 const trakt_api_key = process.env.REACT_APP_TRAKT_API_KEY;
@@ -39,27 +39,26 @@ const IMG_URL = 'https://api.themoviedb.org/3';
 const base_headers = {
   'content-type': content_type,
   'trakt-api-key': trakt_api_key,
-  'trakt-api-version': trakt_api_version
+  'trakt-api-version': trakt_api_version,
 };
 
-// const limitAxios = rateLimit(axios.create(), {
-//   maxRequests: 42,
-//   perMilliseconds: 10000,
-// });
+const limitAxios = rateLimit(axios.create(), {
+  maxRequests: 42,
+  perMilliseconds: 10000,
+});
 
 export const loginApi = (code: string) => {
   return axios.post<Session>(LOGIN_URL, {
     code,
-    // client_secret,
     client_id: trakt_api_key,
     redirect_uri: redirect_url,
-    grant_type: 'authorization_code'
+    grant_type: 'authorization_code',
   });
 };
 
 export const getImgsConfigApi = () => {
   return axios.get<ImgConfig>(
-    `${IMG_URL}/configuration?api_key=${tmbdb_api_key}`
+    `${IMG_URL}/configuration?api_key=${tmbdb_api_key}`,
   );
 };
 
@@ -68,11 +67,8 @@ export const getImgsApi = (id: number, type: ItemType) => {
   if (type === 'show') {
     newType = 'tv';
   }
-  // return limitAxios.get<ImageResponse>(
-  //   `${IMG_URL}/${newType}/${id}/images?api_key=${tmbdb_api_key}`,
-  // );
-  return axios.get<ImageResponse>(
-    `${IMG_URL}/${newType}/${id}/images?api_key=${tmbdb_api_key}`
+  return limitAxios.get<ImageResponse>(
+    `${IMG_URL}/${newType}/${id}/images?api_key=${tmbdb_api_key}`,
   );
 };
 
@@ -80,8 +76,8 @@ export const getApi = <T>(id: number, type: ItemType) => {
   return axios.get<T[]>(
     `${BASE_URL}/search/trakt/${id}?type=${type}&extended=full`,
     {
-      headers: base_headers
-    }
+      headers: base_headers,
+    },
   );
 };
 
@@ -89,8 +85,8 @@ export const getSeasonsApi = (id: number) => {
   return axios.get<Season[]>(
     `${BASE_URL}/shows/${id}/seasons?translations=es`,
     {
-      headers: base_headers
-    }
+      headers: base_headers,
+    },
   );
 };
 
@@ -98,8 +94,8 @@ export const getSeasonEpisodesApi = (id: number, season: number) => {
   return axios.get<Episode[]>(
     `${BASE_URL}/shows/${id}/seasons/${season}?extended=full&translations=es`,
     {
-      headers: base_headers
-    }
+      headers: base_headers,
+    },
   );
 };
 
@@ -109,9 +105,9 @@ export const getProgressApi = (session: Session, id: number) => {
     {
       headers: {
         ...base_headers,
-        Authorization: `Bearer ${session.access_token}`
-      }
-    }
+        Authorization: `Bearer ${session.access_token}`,
+      },
+    },
   );
 };
 
@@ -119,27 +115,27 @@ export const getTranslationsApi = (id: number, type: ItemType) => {
   return axios.get<Translation[]>(
     `${BASE_URL}/${type}s/${id}/translations/es`,
     {
-      headers: base_headers
-    }
+      headers: base_headers,
+    },
   );
 };
 
 export const searchApi = <T>(
   query: string,
   type: string,
-  limit: number = 40
+  limit: number = 40,
 ) => {
   return axios.get<T[]>(
     `${BASE_URL}/search/${type}?query=${query}&extended=full&page=1&limit=${limit}`,
     {
-      headers: base_headers
-    }
+      headers: base_headers,
+    },
   );
 };
 
 export const getWatchedApi = <T extends MovieWatched | ShowWatched>(
   session: Session,
-  type: ItemType
+  type: ItemType,
 ) => {
   const url =
     type === 'movie'
@@ -149,65 +145,65 @@ export const getWatchedApi = <T extends MovieWatched | ShowWatched>(
   return axios.get<T[]>(url, {
     headers: {
       ...base_headers,
-      Authorization: `Bearer ${session.access_token}`
-    }
+      Authorization: `Bearer ${session.access_token}`,
+    },
   });
 };
 
 export const addWatchedApi = (
   item: Episode | Season | Movie,
   session: Session,
-  type: ItemType
+  type: ItemType,
 ) => {
   return axios.post<AddedWatched>(
     `${BASE_URL}/sync/history`,
     {
-      [`${type}s`]: [item]
+      [`${type}s`]: [item],
     },
     {
       headers: {
         ...base_headers,
-        Authorization: `Bearer ${session.access_token}`
-      }
-    }
+        Authorization: `Bearer ${session.access_token}`,
+      },
+    },
   );
 };
 
 export const removeWatchedApi = (
   item: Episode | Season | Movie,
   session: Session,
-  type: ItemType
+  type: ItemType,
 ) => {
   return axios.post<RemovedWatched>(
     `${BASE_URL}/sync/history/remove`,
     {
-      [`${type}s`]: [item]
+      [`${type}s`]: [item],
     },
     {
       headers: {
         ...base_headers,
-        Authorization: `Bearer ${session.access_token}`
-      }
-    }
+        Authorization: `Bearer ${session.access_token}`,
+      },
+    },
   );
 };
 
 export const getWatchlistApi = <T extends MovieWatchlist | ShowWatchlist>(
   session: Session,
-  type: 'movie' | 'show'
+  type: 'movie' | 'show',
 ) => {
   return axios
     .get<T[]>(`${BASE_URL}/sync/watchlist/${type}s?extended=full`, {
       headers: {
         ...base_headers,
-        Authorization: `Bearer ${session.access_token}`
-      }
+        Authorization: `Bearer ${session.access_token}`,
+      },
     })
-    .then(res => {
+    .then((res) => {
       const ordered = res.data.sort(
         (a, b) =>
           (new Date(b.listed_at as string) as any) -
-          (new Date(a.listed_at as string) as any)
+          (new Date(a.listed_at as string) as any),
       );
       res.data = ordered;
       return res;
@@ -217,62 +213,62 @@ export const getWatchlistApi = <T extends MovieWatchlist | ShowWatchlist>(
 export const addWatchlistApi = (
   item: Show | Movie,
   session: Session,
-  type: ItemType
+  type: ItemType,
 ) => {
   return axios.post<AddedWatchlist>(
     `${BASE_URL}/sync/watchlist`,
     {
-      [`${type}s`]: [item]
+      [`${type}s`]: [item],
     },
     {
       headers: {
         ...base_headers,
-        Authorization: `Bearer ${session.access_token}`
-      }
-    }
+        Authorization: `Bearer ${session.access_token}`,
+      },
+    },
   );
 };
 
 export const removeWatchlistApi = (
   item: Show | Movie,
   session: Session,
-  type: ItemType
+  type: ItemType,
 ) => {
   return axios.post<RemovedWatchlist>(
     `${BASE_URL}/sync/watchlist/remove`,
     {
-      [`${type}s`]: [item]
+      [`${type}s`]: [item],
     },
     {
       headers: {
         ...base_headers,
-        Authorization: `Bearer ${session.access_token}`
-      }
-    }
+        Authorization: `Bearer ${session.access_token}`,
+      },
+    },
   );
 };
 
 export const getPeopleApi = (id: number, type: ItemType) => {
   return axios.get<People>(`${BASE_URL}/${type}s/${id}/people`, {
     headers: {
-      ...base_headers
-    }
+      ...base_headers,
+    },
   });
 };
 
 export const getPersonApi = (id: number) => {
   return axios.get<Person>(`${BASE_URL}/people/${id}?extended=full`, {
     headers: {
-      ...base_headers
-    }
+      ...base_headers,
+    },
   });
 };
 
 export const getPersonItemsApi = <T>(person: string, type: ItemType) => {
   return axios.get<T>(`${BASE_URL}/people/${person}/${type}s?extended=full`, {
     headers: {
-      ...base_headers
-    }
+      ...base_headers,
+    },
   });
 };
 
@@ -282,9 +278,9 @@ export const getPopularApi = (type: ItemType, limit: number = 40) => {
     `${BASE_URL}/${type}s/watched/weekly?extended=full&page=1&limit=${limit}&years=${year}`,
     {
       headers: {
-        ...base_headers
-      }
-    }
+        ...base_headers,
+      },
+    },
   );
 };
 
@@ -293,9 +289,9 @@ export const getRelatedApi = <T>(id: number, type: ItemType) => {
     `${BASE_URL}/${type}s/${id}/related?extended=full&page=1&limit=10`,
     {
       headers: {
-        ...base_headers
-      }
-    }
+        ...base_headers,
+      },
+    },
   );
 };
 
@@ -303,8 +299,8 @@ export const getStatsApi = (session: Session) => {
   return axios.get<UserStats>(`${BASE_URL}/users/me/stats`, {
     headers: {
       ...base_headers,
-      Authorization: `Bearer ${session.access_token}`
-    }
+      Authorization: `Bearer ${session.access_token}`,
+    },
   });
 };
 
@@ -312,15 +308,15 @@ export const getProfileApi = (session: Session) => {
   return axios.get<Profile>(`${BASE_URL}/users/me`, {
     headers: {
       ...base_headers,
-      Authorization: `Bearer ${session.access_token}`
-    }
+      Authorization: `Bearer ${session.access_token}`,
+    },
   });
 };
 
 export const getRatingsApi = (id: number, type: ItemType) => {
   return axios.get<Ratings>(`${BASE_URL}/${type}s/${id}/ratings`, {
     headers: {
-      ...base_headers
-    }
+      ...base_headers,
+    },
   });
 };
