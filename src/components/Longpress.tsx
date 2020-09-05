730ea46cf540eae339d713b74b60112aa053f47d
pouyio <pouyio@gmail.com>
2020-09-05 17:31:28 +0200

    :art: added navigation animation

--- src/components/Longpress.tsx
+++ src/components/Longpress.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import LongPressable from 'react-longpressable';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Emoji from './Emoji';
 import { useDispatch } from 'react-redux';
 import { setGlobalSearch } from 'state/store';
 
 const LongPress: React.FC = () => {
   const history = useHistory();
-  const { pathname } = useLocation();
 
   const dispatch = useDispatch();
   return (
@@ -16,11 +15,7 @@ const LongPress: React.FC = () => {
       onLongPress={() => dispatch(setGlobalSearch(true))}
       longPressTime={500}
     >
-      <div
-        className={`flex items-center cursor-pointer ${
-          pathname === '/search' ? 'selected-nav-item' : ''
-        }`}
-      >
+      <div className="flex items-center cursor-pointer">
         <Emoji emoji="🔍" />
         <span className="ml-2 text-base hidden lg:inline">Buscar</span>
       </div>
---
+++ src/components/Navigation/NavigationTabs.tsx
@@ -0,0 +1,128 @@
+import { motion } from 'framer-motion';
+import { useResize } from '../../hooks';
+import React, { useEffect, useRef, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import Emoji from '../Emoji';
+import LongPress from '../Longpress';
+
+export const NavigationTabs: React.FC<{
+  logged: boolean;
+}> = ({ logged }) => {
+  const { pathname } = useLocation();
+  const { bounds, ref } = useResize();
+  const [slider, setSlider] = useState({ animating: true, left: 0, width: 0 });
+  const refs = useRef(
+    new Map<string, HTMLLIElement | null>([
+      ['/shows', null],
+      ['/search', null],
+      ['/calendar', null],
+      ['/profile', null],
+      ['/movies', null],
+    ])
+  );
+
+  useEffect(() => {
+    const target = refs.current.get(pathname);
+    if (target) {
+      const tRect = target.getBoundingClientRect();
+
+      setSlider({
+        animating: true,
+        left: tRect.left,
+        width: tRect.width,
+      });
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    const target = refs.current.get(pathname);
+    if (target) {
+      const tRect = target.getBoundingClientRect();
+
+      setSlider({
+        animating: false,
+        left: tRect.left,
+        width: tRect.width,
+      });
+    }
+  }, [bounds]);
+
+  return (
+    <>
+      <ul
+        className="flex justify-around px-2 text-center bg-gray-200"
+        style={{ paddingBottom: 'env(safe-area-inset-bottom)' }}
+        ref={ref}
+      >
+        {logged ? (
+          <>
+            <li
+              className="py-1"
+              onClick={() => window.scrollTo(0, 0)}
+              ref={(el) => refs.current.set('/movies', el)}
+            >
+              <NavLink
+                to="/movies?mode=watchlist&page=1"
+                className="flex items-center"
+              >
+                <Emoji emoji="🎬" />
+                <span className="ml-2 text-base hidden lg:inline">
+                  Películas
+                </span>
+              </NavLink>
+            </li>
+            <li
+              className="py-1"
+              onClick={() => window.scrollTo(0, 0)}
+              ref={(el) => refs.current.set('/shows', el)}
+            >
+              <NavLink
+                to="/shows?mode=watched&page=1"
+                className="flex items-center"
+              >
+                <Emoji emoji="📺" />
+                <span className="ml-2 text-base hidden lg:inline">Series</span>
+              </NavLink>
+            </li>
+          </>
+        ) : null}
+        <li
+          className="py-1"
+          onClick={() => window.scrollTo(0, 0)}
+          ref={(el) => refs.current.set('/search', el)}
+        >
+          <LongPress />
+        </li>
+        {logged ? (
+          <li
+            className="py-1"
+            onClick={() => window.scrollTo(0, 0)}
+            ref={(el) => refs.current.set('/calendar', el)}
+          >
+            <NavLink to="/calendar" className="flex items-center">
+              <Emoji emoji="📅" />
+              <span className="ml-2 text-base hidden lg:inline">Calendar</span>
+            </NavLink>
+          </li>
+        ) : null}
+        <li className="py-1" ref={(el) => refs.current.set('/profile', el)}>
+          <NavLink to="/profile" className="flex items-center">
+            <Emoji emoji="👤" />
+            <span className="ml-2 text-base hidden lg:inline">Perfil</span>
+          </NavLink>
+        </li>
+      </ul>
+      <motion.div
+        className="absolute bg-gray-600 bottom-0 rounded"
+        static={!slider.animating}
+        initial={false}
+        animate={{
+          height: '2px',
+          bottom: '4px',
+          left: slider.left,
+          width: slider.width,
+        }}
+      />
+    </>
+  );
+};
