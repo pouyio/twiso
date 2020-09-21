80ba5311f969ac35dcdf7f74c1ac9e32930348b7
Vicente <pouyio@gmail.com>
2020-09-21 09:40:40 +0200

    :art: modal overlay

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -17,7 +17,7 @@ const Modal: React.FC<IModalProps> = ({ modalRef }) => {
         isShowing ? '' : 'hidden'
       } fixed w-full h-full top-0 left-0 flex items-center justify-center z-10`}
     >
-      <div className="absolute w-full h-full bg-gray-900 opacity-50"></div>
+      <div className="absolute w-full h-full bg-gray-600 opacity-50"></div>
 
       <div
         className="text-black bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto"
