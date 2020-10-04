403110559b92fab6ec008263492cccdfd11af34c
pouyio <pouyio@gmail.com>
2020-10-04 23:03:34 +0200

    :art: gradient pulse on loading direction

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -91,7 +91,7 @@ const People: React.FC<IPeopleProps> = ({ people, type }) => {
               )
             ) : (
               <li className="my-1">
-                <div className="my-1 bg-gray-200 font-light rounded-full mx-1 whitespace-pre flex flex-col flex-shrink-0 text-center w-40 h-10"></div>
+                <div className="my-1 bg-gray-200 font-light rounded-full mx-1 whitespace-pre flex flex-col flex-shrink-0 text-center w-40 h-10 bg-gradient-to-r animate-pulse"></div>
               </li>
             )}
           </ul>
