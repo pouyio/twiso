3ba371f6a968299c4a0f099d09bffb49f4a180d0
vicente <pouyio@gmail.com>
2021-08-01 14:42:08 +0200

    replaced table names with consts

--- src/state/middleware.ts
+++ src/state/middleware.ts
@@ -1,11 +1,14 @@
 import { Middleware } from '@reduxjs/toolkit';
 import db from '../utils/db';
 
+const MOVIES = 'movies';
+const SHOWS = 'shows';
+
 export const dbMiddleware: Middleware = (store) => (next) => (action) => {
   switch (action.type) {
     case 'movies/addWatched/fulfilled': {
       if (action.payload.added.movies) {
-        db.table('movies').put({
+        db.table(MOVIES).put({
           movie: action.meta.arg.movie,
           localState: 'watched',
         });
@@ -15,13 +18,13 @@ export const dbMiddleware: Middleware = (store) => (next) => (action) => {
     case 'movies/removeWatched/fulfilled':
     case 'movies/removeWatchlist/fulfilled': {
       if (action.payload.deleted.movies) {
-        db.table('movies').delete(action.meta.arg.movie.ids.trakt);
+        db.table(MOVIES).delete(action.meta.arg.movie.ids.trakt);
       }
       break;
     }
     case 'movies/addWatchlist/fulfilled': {
       if (action.payload.added.movies) {
-        db.table('movies').put({
+        db.table(MOVIES).put({
           movie: action.meta.arg.movie,
           localState: 'watchlist',
         });
@@ -29,9 +32,7 @@ export const dbMiddleware: Middleware = (store) => (next) => (action) => {
       break;
     }
     case 'movies/removeWatchlists': {
-      db.table('movies').bulkDelete(
-        action.payload.map((m) => m.movie.ids.trakt)
-      );
+      db.table(MOVIES).bulkDelete(action.payload.map((m) => m.movie.ids.trakt));
       break;
     }
     case 'movies/getMovie/fulfilled': {
@@ -40,16 +41,16 @@ export const dbMiddleware: Middleware = (store) => (next) => (action) => {
         (m) => m.movie.ids.trakt === action.meta.arg.id
       );
       const newMovie = { ...oldMovie, movie: action.payload };
-      db.table('movies').put({ ...newMovie, localState: action.meta.arg.type });
+      db.table(MOVIES).put({ ...newMovie, localState: action.meta.arg.type });
       break;
     }
     case 'shows/removeWatchlists': {
-      db.table('shows').bulkDelete(action.payload.map((s) => s.show.ids.trakt));
+      db.table(SHOWS).bulkDelete(action.payload.map((s) => s.show.ids.trakt));
       break;
     }
     case 'shows/addWatchlist/fulfilled': {
       if (action.payload.added.shows) {
-        db.table('shows').put({
+        db.table(SHOWS).put({
           show: action.meta.arg.show,
           localState: 'watchlist',
         });
@@ -57,43 +58,43 @@ export const dbMiddleware: Middleware = (store) => (next) => (action) => {
       break;
     }
     case 'shows/removeWatcheds': {
-      db.table('shows').bulkDelete(action.payload.map((s) => s.show.ids.trakt));
+      db.table(SHOWS).bulkDelete(action.payload.map((s) => s.show.ids.trakt));
       break;
     }
     case 'shows/_removeWatched': {
-      db.table('shows').delete(action.payload);
+      db.table(SHOWS).delete(action.payload);
       break;
     }
     case 'shows/removeWatchlist/fulfilled': {
       if (action.payload.deleted.shows) {
-        db.table('shows').delete(action.meta.arg.show.ids.trakt);
+        db.table(SHOWS).delete(action.meta.arg.show.ids.trakt);
       }
       break;
     }
     // TODO remove if possible all _removewatchlist can be deleted
     case 'shows/_removeWatchlist': {
-      db.table('shows').delete(action.payload);
+      db.table(SHOWS).delete(action.payload);
       break;
     }
     case 'shows/updateShow': {
-      db.table('shows').update(action.payload.show.ids.trakt, {
+      db.table(SHOWS).update(action.payload.show.ids.trakt, {
         ...action.payload,
       });
       break;
     }
     case 'shows/addWatched': {
-      db.table('shows').put({ ...action.payload, localState: 'watched' });
+      db.table(SHOWS).put({ ...action.payload, localState: 'watched' });
       break;
     }
     case 'shows/updateProgress': {
-      db.table('shows').update(action.payload.show.show.ids.trakt, {
+      db.table(SHOWS).update(action.payload.show.show.ids.trakt, {
         progress: action.payload.progress,
         last_watched_at: action.payload.progress.last_watched_at,
       });
       break;
     }
     case 'shows/updateSeasons': {
-      db.table('shows').update(action.payload.show.show.ids.trakt, {
+      db.table(SHOWS).update(action.payload.show.show.ids.trakt, {
         fullSeasons: action.payload.seasons,
       });
       break;
@@ -104,18 +105,18 @@ export const dbMiddleware: Middleware = (store) => (next) => (action) => {
         (s) => s.show.ids.trakt === action.meta.arg.id
       );
       const newShow = { ...oldShow, show: action.payload };
-      db.table('shows').put({ ...newShow, localState: action.meta.arg.type });
+      db.table(SHOWS).put({ ...newShow, localState: action.meta.arg.type });
       break;
     }
     case 'shows/updateFullShow/fulfilled': {
-      db.table('shows').update(
+      db.table(SHOWS).update(
         action.meta.arg.outdated.show.ids.trakt,
         action.payload
       );
       break;
     }
     case 'shows/addEpisodeWatched/fulfilled': {
-      db.table('shows').put({
+      db.table(SHOWS).put({
         ...action.payload,
         localState: 'watched',
       });
@@ -123,12 +124,12 @@ export const dbMiddleware: Middleware = (store) => (next) => (action) => {
     }
     case 'shows/removeEpisodeWatched/fulfilled': {
       if (action.payload) {
-        db.table('shows').update(action.meta.arg.show.show.ids.trakt, {
+        db.table(SHOWS).update(action.meta.arg.show.show.ids.trakt, {
           progress: action.payload,
           last_watched_at: action.payload.last_watched_at,
         });
       } else {
-        db.table('shows').delete(action.meta.arg.show.show.ids.trakt);
+        db.table(SHOWS).delete(action.meta.arg.show.show.ids.trakt);
       }
       break;
     }
