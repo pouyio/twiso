06c98873360de37a64f5b2f635b9cd25feba2db3
pouyio <pouyio@gmail.com>
2023-03-26 12:50:03 +0200

    :rocket: fix server

---
+++ api/server.tsx
@@ -0,0 +1,31 @@
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(req: Request) {
+  console.log('EXECUTED');
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Hello world!
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    }
+  );
+}
