85f585fdeb5188337f4353f2aa92938a9be9f247
pouyio <pouyio@gmail.com>
2023-03-29 12:53:48 +0200

    :rocket: fix server

--- api/server.ts
+++ api/server.ts
@@ -6,10 +6,9 @@ type ItemType = any;
 type ImageResponse = any;
 type BaseImage = any;
 import { config } from 'https://deno.land/x/dotenv@v3.2.2/mod.ts';
-import express from 'npm:express@4.17.1';
+// import express from 'npm:express@4.17.1';
 // import path from 'node:path';
 // import fs from 'node:fs';
-import axios from 'npm:axios@0.21.2';
 // const __dirname = new URL('.', import.meta.url).pathname;
 
 export const CONTENT_TYPE = 'application/json';
@@ -18,43 +17,58 @@ export const BASE_URL = 'https://api.trakt.tv';
 export const LOGIN_URL = 'https://trakt.tv/oauth/token';
 export const IMG_URL = 'https://api.themoviedb.org/3';
 
-const app = express();
+// const app = express();
 
-const ROUTES = {
-  shows: '/shows',
-  movies: '/movies',
-  search: '/search',
-  calendar: '/calendar',
-  profile: '/profile',
-};
+// const ROUTES = {
+//   shows: '/shows',
+//   movies: '/movies',
+//   search: '/search',
+//   calendar: '/calendar',
+//   profile: '/profile',
+// };
 
-const ROUTE = {
-  movie: '/movie/:id',
-  show: '/show/:id',
-  person: '/person/:id',
-};
+// const ROUTE = {
+//   movie: '/movie/:id',
+//   show: '/show/:id',
+//   person: '/person/:id',
+// };
 const findFirstValid = (images: BaseImage[], language: string) => {
   const p = images.find((p) => p.iso_639_1 === language);
   return p || images.find((p) => p.iso_639_1 === 'en') || images[0];
 };
 
-const tracktClient = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'content-type': CONTENT_TYPE,
-    'trakt-api-key': config().VITE_TRAKT_API_KEY,
-    'trakt-api-version': TRAKT_API_VERSION,
+const tracktClient = {
+  get: <T>(path: string) => {
+    return fetch(BASE_URL + path, {
+      headers: {
+        'content-type': CONTENT_TYPE,
+        'trakt-api-key': config().VITE_TRAKT_API_KEY,
+        'trakt-api-version': TRAKT_API_VERSION,
+      },
+    })
+      .then((r) => r.json())
+      .then<T[]>((r) => ({ data: r }));
   },
-});
+};
+// const tracktClient = axios.create({
+//   baseURL: BASE_URL,
+//   headers: {
+//     'content-type': CONTENT_TYPE,
+//     'trakt-api-key': config().VITE_TRAKT_API_KEY,
+//     'trakt-api-version': TRAKT_API_VERSION,
+//   },
+// });
 
 export const getImgsApi = (id: number, type: ItemType) => {
   let newType: string = type;
   if (type === 'show') {
     newType = 'tv';
   }
-  return axios.get<ImageResponse>(
+  return fetch(
     `${IMG_URL}/${newType}/${id}/images?api_key=${config().VITE_TMDB_API_KEY}`
-  );
+  )
+    .then((r) => r.json())
+    .then<ImageResponse>((r) => ({ data: r }));
 };
 
 const fetchData = async <T extends SearchMovie | SearchShow | SearchPerson>(
@@ -110,22 +124,24 @@ const fetchData = async <T extends SearchMovie | SearchShow | SearchPerson>(
 //   );
 // });
 
-app.get(ROUTE.movie, async (req, res) => {
-  const data = Deno.readTextFileSync('./../build/index.html');
-  const { item, imgUrl } = await fetchData<SearchMovie>(
-    'movie',
-    +req.params.id
-  );
+const app = async (req: Request) => {
+  const data = Deno.readTextFileSync('./build/index.html');
+  const id = new URL(req.url).searchParams.get('id') ?? '';
+  const { item, imgUrl } = await fetchData<SearchMovie>('movie', +id);
 
   const finalData = data
     .replace('__OG_TYPE__', 'video.movie')
     .replace('__OG_TITLE__', item?.title)
     .replace('__OG_IMAGE__', imgUrl)
-    .replace(/__OG_URL__/, `https://twiso.now.sh${req.path}`)
+    .replace('/__OG_URL__/', `https://twiso.now.sh${req.path}`)
     .replace('__OG_DESCRIPTION__', item?.overview);
 
-  res.send(finalData);
-});
+  return new Response(finalData, {
+    headers: {
+      'content-type': 'text/html',
+    },
+  });
+};
 
 // app.get(ROUTE.show, (req, res) => {
 //   fs.readFile(
