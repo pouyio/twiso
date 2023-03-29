a35048d22ee9b31ce5cd591b7042103f9933466a
pouyio <pouyio@gmail.com>
2023-03-29 14:45:39 +0200

    :rocket: fix server

--- api/server.ts
+++ api/server.ts
@@ -127,7 +127,7 @@ const fetchData = async <T extends SearchMovie | SearchShow | SearchPerson>(
 
 const app = async (req: Request) => {
   	
-  const data = Deno.readTextFileSync('index.html');
+  const data = await Deno.readTextFile('index.html');
   const id = new URL(req.url).searchParams.get('id') ?? '';
   const { item, imgUrl } = await fetchData<SearchMovie>('movie', +id);
 
