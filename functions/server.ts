ce6d20b676867a646d62750b0ba621155b4224fc
pouyio <pouyio@gmail.com>
2023-03-30 17:32:28 +0200

    :rocket: fix server

--- functions/server.ts
+++ functions/server.ts
@@ -1,25 +1,38 @@
-type SearchMovie = any;
-type SearchShow = any;
-type SearchPerson = any;
-type ItemType = any;
-type ImageResponse = any;
-type BaseImage = any;
-// import { ROUTES, ROUTE } from '../src/utils/routes';
-// import express from 'express';
+// type SearchMovie = any;
+// type SearchShow = any;
+// type SearchPerson = any;
+// type ItemType = any;
+// type ImageResponse = any;
+// type BaseImage = any;
+// const CONTENT_TYPE = 'application/json';
+// const TRAKT_API_VERSION = '2';
+// const BASE_URL = 'https://api.trakt.tv';
+// const LOGIN_URL = 'https://trakt.tv/oauth/token';
+// const IMG_URL = 'https://api.themoviedb.org/3';
+
+import { Handler, HandlerEvent } from '@netlify/functions';
 import axios from 'axios';
-const path = require('path');
-const fs = require('fs');
-// const app = express();
-export const CONTENT_TYPE = 'application/json';
-export const TRAKT_API_VERSION = '2';
-export const BASE_URL = 'https://api.trakt.tv';
-export const LOGIN_URL = 'https://trakt.tv/oauth/token';
-export const IMG_URL = 'https://api.themoviedb.org/3';
+import fs from 'fs';
+import {
+  BaseImage,
+  ImageResponse,
+  ItemType,
+  SearchMovie,
+  SearchPerson,
+  SearchShow,
+} from 'models';
+import path from 'path';
+import {
+  BASE_URL,
+  CONTENT_TYPE,
+  IMG_URL,
+  TRAKT_API_VERSION,
+} from 'utils/apiConfig';
 const findFirstValid = (images: BaseImage[], language: string) => {
   const p = images.find((p) => p.iso_639_1 === language);
   return p || images.find((p) => p.iso_639_1 === 'en') || images[0];
 };
-import { Handler, HandlerEvent } from '@netlify/functions';
+
 const tracktClient = {
   get: <T>(path: string) => {
     return axios.get(BASE_URL + path, {
@@ -30,6 +43,22 @@ const tracktClient = {
       },
     });
   },
+};
+
+const TYPE_MAP = {
+  movie: 'video.movie',
+  show: 'video.tv_show',
+  person: 'video.profile',
+};
+
+const getImgsApi = (id: number, type: ItemType) => {
+  let newType: string = type;
+  if (type === 'show') {
+    newType = 'tv';
+  }
+  return axios.get<ImageResponse>(
+    `${IMG_URL}/${newType}/${id}/images?api_key=${process.env.VITE_TMDB_API_KEY}`
+  );
 };
 
 const fetchData = async <T extends SearchMovie | SearchShow | SearchPerson>(
@@ -55,132 +84,6 @@ const fetchData = async <T extends SearchMovie | SearchShow | SearchPerson>(
     imgUrl = `https://image.tmdb.org/t/p/w185${poster.file_path}`;
   }
   return { item, imgUrl };
-};
-
-export const getImgsApi = (id: number, type: ItemType) => {
-  let newType: string = type;
-  if (type === 'show') {
-    newType = 'tv';
-  }
-  return axios.get(
-    `${IMG_URL}/${newType}/${id}/images?api_key=${process.env.VITE_TMDB_API_KEY}`
-  );
-};
-
-// app.get(['/', ...Object.values(ROUTES)], (req, res) => {
-//   fs.readFile(
-//     path.join(__dirname + '/../build/index.html'),
-//     'utf8',
-//     async (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         res.status(404).send();
-//         return;
-//       }
-//       data = data
-//         .replace('__OG_TYPE__', 'website')
-//         .replace('<meta property="og:title" content="__OG_TITLE__"/>', '')
-//         .replace('<meta property="og:image" content="__OG_IMAGE__"/>', '')
-//         .replace(
-//           '<meta property="og:description" content="__OG_DESCRIPTION__"/>',
-//           ''
-//         )
-//         .replace('__OG_URL__', `https://twiso.vercel.app${req.path}`);
-
-//       res.send(data);
-//     }
-//   );
-// });
-
-// app.get(ROUTE.movie, (req, res) => {
-//   fs.readFile(
-//     path.join(__dirname + '/../build/index.html'),
-//     'utf8',
-//     async (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         res.status(404).send('There was a problem 😿');
-//         return;
-//       }
-
-//       const { item, imgUrl } = await fetchData<SearchMovie>(
-//         'movie',
-//         +req.params.id
-//       );
-
-//       const finalData = data
-//         .replace('__OG_TYPE__', 'video.movie')
-//         .replace('__OG_TITLE__', item?.title)
-//         .replace('__OG_IMAGE__', imgUrl)
-//         .replace(/__OG_URL__/, `https://twiso.now.sh${req.path}`)
-//         .replace('__OG_DESCRIPTION__', item?.overview);
-
-//       res.send(finalData);
-//     }
-//   );
-// });
-
-// app.get(ROUTE.show, (req, res) => {
-//   fs.readFile(
-//     path.join(__dirname + '/../build/index.html'),
-//     'utf8',
-//     async (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         res.status(404).send('There was a problem 😿');
-//         return;
-//       }
-
-//       const { item, imgUrl } = await fetchData<SearchShow>(
-//         'show',
-//         +req.params.id
-//       );
-
-//       const finalData = data
-//         .replace('__OG_TYPE__', 'video.tv_show')
-//         .replace('__OG_TITLE__', item?.title)
-//         .replace('__OG_IMAGE__', imgUrl)
-//         .replace(/__OG_URL__/, `https://twiso.now.sh${req.path}`)
-//         .replace('__OG_DESCRIPTION__', item?.overview);
-
-//       res.send(finalData);
-//     }
-//   );
-// });
-
-// app.get(ROUTE.person, (req, res) => {
-//   fs.readFile(
-//     path.join(__dirname + '/../build/index.html'),
-//     'utf8',
-//     async (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         res.status(404).send('There was a problem 😿');
-//         return;
-//       }
-
-//       const { item, imgUrl } = await fetchData<SearchPerson>(
-//         'person',
-//         +req.params.id
-//       );
-
-//       const finalData = data
-//         .replace('__OG_TYPE__', 'profile')
-//         .replace('__OG_TITLE__', item.name)
-//         .replace('__OG_IMAGE__', imgUrl)
-//         .replace('__OG_URL__', `https://twiso.now.sh${req.path}`)
-//         .replace('__OG_DESCRIPTION__', item.biography);
-
-//       res.send(finalData);
-//     }
-//   );
-// });
-
-// app.use(express.static(path.join(__dirname, '../build')));
-const TYPE_MAP = {
-  movie: 'video.movie',
-  show: 'video.tv_show',
-  person: 'video.profile',
 };
 
 const handler: Handler = async (req: HandlerEvent) => {
