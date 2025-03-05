a9c34f42b784c447da9fbe59071d575e9fee8bac
Vicente <pouyio@gmail.com>
2025-03-05 23:22:47 +0100

    fix

--- server.ts
+++ server.ts
@@ -1,15 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import { findFirstValid } from '../src/utils/findFirstValidImage';
-import { SearchMovie, SearchPerson, SearchShow } from '../src/models/Movie';
+import { findFirstValid } from './src/utils/findFirstValidImage';
+import { SearchMovie, SearchPerson, SearchShow } from './src/models/Movie';
 import {
   BASE_URL,
   CONTENT_TYPE,
   IMG_URL,
   TRAKT_API_VERSION,
-} from '../src/utils/apiConsts';
+} from './src/utils/apiConsts';
 import { EventContext } from '@cloudflare/workers-types';
-import { ImageResponse } from 'models/Image';
-import { ItemType } from 'models/ItemType';
+import { ImageResponse } from './src/models/Image';
+import { ItemType } from './src/models/ItemType';
 
 type ENVs = {
   VITE_TRAKT_API_KEY: string;
@@ -77,43 +77,41 @@ const fetchData = async <T extends SearchMovie | SearchShow | SearchPerson>(
 };
 
 export const onRequest = async (context: EventContext<ENVs, any, any>) => {
-  console.log(context.params);
-  return new Response('ok!');
-  // const { request, env } = context;
-  // const url = new URL(request.url);
-  // const parts = url.pathname.split('/').filter(Boolean); // Remove empty parts
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const parts = url.pathname.split('/').filter(Boolean); // Remove empty parts
 
-  // if (parts.length < 2) {
-  //   return new Response('Invalid request', { status: 400 });
-  // }
+  if (parts.length < 2) {
+    return new Response('Invalid request', { status: 400 });
+  }
 
-  // const type = parts[0] as 'movie' | 'person' | 'show';
-  // const id = parseInt(parts[1]);
+  const type = parts[0] as 'movie' | 'person' | 'show';
+  const id = parseInt(parts[1]);
 
-  // if (isNaN(id)) {
-  //   return new Response('Invalid ID', { status: 400 });
-  // }
+  if (isNaN(id)) {
+    return new Response('Invalid ID', { status: 400 });
+  }
 
-  // const { item, imgUrl } = await fetchData<SearchMovie>(type, id, env);
+  const { item, imgUrl } = await fetchData<SearchMovie>(type, id, env);
 
-  // // Load your index.html file (Cloudflare Workers does not support fs.readFileSync)
-  // const indexHtml = await fetch(new Request(`${url.origin}/index.html`));
-  // let data = await indexHtml.text();
+  // Load your index.html file (Cloudflare Workers does not support fs.readFileSync)
+  const indexHtml = await fetch(new Request(`${url.origin}/index.html`));
+  let data = await indexHtml.text();
 
-  // // Replace meta tags
-  // const finalData = data.replace(
-  //   '<!-- __META_OG__ -->',
-  //   `<meta property="og:type" content="${TYPE_MAP[type]}" />
-  //   <meta property="og:title" content="${item?.title}" />
-  //   <meta property="og:image" content="${imgUrl}" />
-  //   <meta property="og:url" content="https://twiso.pages.dev${url.pathname}" />
-  //   <meta property="og:description" content="${item?.overview}" />
-  //   `
-  // );
+  // Replace meta tags
+  const finalData = data.replace(
+    '<!-- __META_OG__ -->',
+    `<meta property="og:type" content="${TYPE_MAP[type]}" />
+    <meta property="og:title" content="${item?.title}" />
+    <meta property="og:image" content="${imgUrl}" />
+    <meta property="og:url" content="https://twiso.pages.dev${url.pathname}" />
+    <meta property="og:description" content="${item?.overview}" />
+    `
+  );
 
-  // return new Response(finalData, {
-  //   headers: {
-  //     'Content-Type': 'text/html; charset=UTF-8',
-  //   },
-  // });
+  return new Response(finalData, {
+    headers: {
+      'Content-Type': 'text/html; charset=UTF-8',
+    },
+  });
 };
