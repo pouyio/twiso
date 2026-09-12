e54a52337682144534994a8ba2107430ee460bae
Vicente <pouyio@gmail.com>
2026-09-12 19:21:13 +0200

    :bug: fix long press in ios

--- src/components/Longpress.tsx
+++ src/components/Longpress.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { useNavigate } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { setGlobalSearch } from '../state/slices/root';
@@ -11,13 +11,30 @@ const LongPress: React.FC = () => {
   const navigate = useNavigate();
   const { t } = useTranslate();
   const { session } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const suppressClick = useRef(false);
   const handlers = useLongPress(
-    () => !!session && dispatch(setGlobalSearch(true))
+    () => {
+      if (!session) return;
+      suppressClick.current = true;
+      dispatch(setGlobalSearch(true));
+    },
+    { threshold: 400 }
   );
 
-  const dispatch = useDispatch();
   return (
-    <button {...handlers()} onClick={() => navigate('/search')}>
+    <button
+      {...handlers()}
+      className="select-none [-webkit-touch-callout:none]"
+      onContextMenu={(e) => e.preventDefault()}
+      onClick={() => {
+        if (suppressClick.current) {
+          suppressClick.current = false;
+          return;
+        }
+        navigate('/search');
+      }}
+    >
       <div className="flex items-center cursor-pointer">
         <Icon name="search" className="h-8" />
         <span className="ml-2 text-base hidden lg:inline capitalize">
